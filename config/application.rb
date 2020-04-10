726118d3ce2fb6585d19b4cadbe3b669b922b152
Ife Odugbesan <ifeodugbesan@yahoo.com>
2020-04-10 20:28:30 +0100

    added home page

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,7 @@ module Levoyage
         end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+    config.assets.paths << Rails.root.join('vendor', 'assets', 'fonts')
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
