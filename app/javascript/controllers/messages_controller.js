c4b7b096a59d9eb77565c95b05ab2763e9e08b96
Ife Odugbesan <ifeodugbesan@yahoo.com>
2020-05-13 19:56:49 +0100

    added chat responsiveness

--- app/javascript/controllers/messages_controller.js
+++ app/javascript/controllers/messages_controller.js
@@ -3,40 +3,50 @@ import Pusher from 'pusher-js';
 import $ from 'jquery'
 
 export default class extends Controller {
-  static targets = ["container", "messages", "form"]
+  static targets = ["container", "messages", "form", "mobile", "input"]
 
   connect() {
-    const container = this.containerTarget
-    const messages = this.messagesTarget
-    messages.scrollTop = messages.scrollHeight;
-    const key = this.containerTarget.dataset.key
-    const currentUser = this.containerTarget.dataset.currentUser
-    const cluster = this.containerTarget.dataset.cluster
-    let pusher = new Pusher(key, {
-      cluster: cluster,
-      forceTLS: true
-    });
-    let channel = pusher.subscribe('my-channel');
-    channel.bind('my-event', function(data) {
-      if (data.user_id == currentUser) {
-        return;
+    if (this.hasMobileTarget) {
+      if (navigator.standalone) {
+        this.mobileTarget.style.height = `${window.innerHeight - 140}px`
+      } else {
+        this.mobileTarget.style.height = `${window.innerHeight - 66}px`
       }
-      let newEntry = `
-                      <div class="each-message py-1 justify-content-start">
-                        <img src="https://res.cloudinary.com/${data.cloud}/image/upload/${data.avatar}" class="avatar-large" alt="user-avatar">
-                        <div class="" style="max-width: 77%;">
-                          <div class="message-body ml-2 speech-left">
-                            ${data.body}
+    }
+    if (this.hasContainerTarget) {
+      const container = this.containerTarget
+      const messages = this.messagesTarget
+      messages.scrollTop = messages.scrollHeight;
+      const key = this.containerTarget.dataset.key
+      const currentUser = this.containerTarget.dataset.currentUser
+      const cluster = this.containerTarget.dataset.cluster
+      let pusher = new Pusher(key, {
+        cluster: cluster,
+        forceTLS: true
+      });
+      let channel = pusher.subscribe('my-channel');
+      channel.bind('my-event', function(data) {
+        if (data.user_id == currentUser) {
+          return;
+        }
+        let newEntry = `
+                        <div class="each-message py-1 justify-content-start">
+                          <img src="https://res.cloudinary.com/${data.cloud}/image/upload/${data.avatar}" class="avatar-large" alt="user-avatar">
+                          <div class="" style="max-width: 77%;">
+                            <div class="message-body ml-2 speech-left">
+                              ${data.body}
+                            </div>
+                            <p class="time-created tiny-text lighter mr-3 mb-0 text-right">now</p>
                           </div>
-                          <p class="time-created tiny-text lighter mr-3 mb-0 text-right">now</p>
                         </div>
-                      </div>
-                      `
-      messages.insertAdjacentHTML('beforeend', newEntry);
-      setTimeout(function() {
-        document.querySelector('.time-created').innerText = `${data.created_at}`
-      }, 10000)
-    });
+                        `
+        messages.insertAdjacentHTML('beforeend', newEntry);
+        setTimeout(function() {
+          let time = document.querySelectorAll('.time-created');
+          time[time.length - 1].innerText = `${data.created_at}`;
+        }, 10000)
+      });
+    }
   }
 
   newMessage() {
@@ -46,4 +56,13 @@ export default class extends Controller {
     this.formTarget.reset();
     $(messages).animate({ scrollTop: messages.scrollHeight }, 300);
   }
+
+  updateMessageBox(event) {
+    event.target.style.height = `1px`;
+    event.target.style.height = `${event.target.scrollHeight}px`;
+  }
 }
+
+// 1. SET HEIGHT OF MESSAGE BOX WHEN ITS PWA AND EXTRA BOTTOM BIT PWA
+// 2. CONVERSATIONS PAGE FOR WEB, AJAX FOR MESSAGES ON SAME PAGE
+// 3. NOTIFICATIONS FOR NEW MESSAGE
