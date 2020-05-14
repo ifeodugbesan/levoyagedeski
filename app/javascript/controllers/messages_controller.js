c4ff6815b96a8bc712725f5a821808931358fc1b
Ife Odugbesan <ifeodugbesan@yahoo.com>
2020-05-14 19:00:06 +0100

    fixed condition in connect

--- app/javascript/controllers/messages_controller.js
+++ app/javascript/controllers/messages_controller.js
@@ -13,7 +13,7 @@ export default class extends Controller {
         this.mobileTarget.style.height = `${window.innerHeight - 66}px`
       }
     }
-    if (this.hasContainerTarget) {
+    if (this.hasContainerTarget && this.hasMessagesTarget) {
       const container = this.containerTarget
       const messages = this.messagesTarget
       messages.scrollTop = messages.scrollHeight;
