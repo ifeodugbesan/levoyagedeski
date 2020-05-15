a704a1a01714d048566e2369fb3644909a9475e3
Ife Odugbesan <ifeodugbesan@yahoo.com>
2020-05-15 17:35:03 +0100

    updated pusher, added date and notifications for chat

--- app/javascript/controllers/messages_controller.js
+++ app/javascript/controllers/messages_controller.js
@@ -3,7 +3,7 @@ import Pusher from 'pusher-js';
 import $ from 'jquery'
 
 export default class extends Controller {
-  static targets = ["container", "messages", "form", "mobile", "input", "chatContainer", "suggestions"]
+  static targets = ["container", "messages", "form", "mobile", "input", "chatContainer", "suggestions", "navChat"]
 
   connect() {
     if (this.hasMobileTarget) {
@@ -14,25 +14,25 @@ export default class extends Controller {
       }
     }
     if (this.hasContainerTarget && this.hasMessagesTarget) {
-      const container = this.containerTarget
-      const messages = this.messagesTarget
+      let container = this.containerTarget
+      let messages = this.messagesTarget
       messages.scrollTop = messages.scrollHeight;
-      const key = this.containerTarget.dataset.key
-      const currentUser = this.containerTarget.dataset.currentUser
-      const cluster = this.containerTarget.dataset.cluster
-      const chatChannel = this.containerTarget.dataset.channel
+      let key = this.containerTarget.dataset.key
+      let currentUser = this.containerTarget.dataset.currentUser
+      let cluster = this.containerTarget.dataset.cluster
+      let chatChannel = this.containerTarget.dataset.channel
       let pusher = new Pusher(key, {
         cluster: cluster,
         forceTLS: true
       });
       let channel = pusher.subscribe(chatChannel);
-      channel.bind('my-event', function(data) {
+      channel.bind('new-message', function(data) {
         if (data.user_id == currentUser) {
           return;
         }
         let newEntry = `
                         <div class="each-message py-1 justify-content-start">
-                          <img src="https://res.cloudinary.com/${data.cloud}/image/upload/${data.avatar}" class="avatar-large" alt="user-avatar">
+                          <img src="https://res.cloudinary.com/${data.cloud}/image/upload/${data.avatar}" class="user-avatar" alt="user-avatar">
                           <div class="" style="max-width: 77%;">
                             <div class="message-body ml-2 speech-left">
                               ${data.body}
@@ -42,10 +42,12 @@ export default class extends Controller {
                         </div>
                         `
         messages.insertAdjacentHTML('beforeend', newEntry);
-        setTimeout(function() {
-          let time = document.querySelectorAll('.time-created');
-          time[time.length - 1].innerText = `${data.created_at}`;
-        }, 10000)
+        let time = document.querySelectorAll('.time-created');
+        if (time) {
+          setTimeout(function() {
+            time[time.length - 1].innerText = `${data.created_at}`;
+          }, 10000)
+        }
       });
     }
   }
@@ -63,12 +65,13 @@ export default class extends Controller {
     event.target.style.height = `${event.target.scrollHeight}px`;
   }
 
-  switchConversation() {
+  switchConversation(event) {
     let [data, status, xhr] = event.detail;
     let chatContainer = this.chatContainerTarget
     chatContainer.innerHTML = xhr.response
-    const messages = this.messagesTarget
+    let messages = this.messagesTarget
     messages.scrollTop = messages.scrollHeight;
+    event.target.firstElementChild.firstElementChild.classList.remove('border-red');
   }
 
   searchResults() {
