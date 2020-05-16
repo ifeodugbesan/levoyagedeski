7e4059f317b19a4d27a8f4bf392f9920286ed848
Ife Odugbesan <ifeodugbesan@yahoo.com>
2020-05-16 12:47:51 +0100

    added chats stimulus controller and moved messages controller into the partial

--- app/javascript/controllers/messages_controller.js
+++ app/javascript/controllers/messages_controller.js
@@ -3,16 +3,9 @@ import Pusher from 'pusher-js';
 import $ from 'jquery'
 
 export default class extends Controller {
-  static targets = ["container", "messages", "form", "mobile", "input", "chatContainer", "suggestions", "navChat"]
+  static targets = ["container", "messages", "form", "input"]
 
   connect() {
-    if (this.hasMobileTarget) {
-      if (navigator.standalone) {
-        this.mobileTarget.style.height = `${window.innerHeight - 140}px`
-      } else {
-        this.mobileTarget.style.height = `${window.innerHeight - 66}px`
-      }
-    }
     if (this.hasContainerTarget && this.hasMessagesTarget) {
       let container = this.containerTarget
       let messages = this.messagesTarget
@@ -63,19 +56,6 @@ export default class extends Controller {
   updateMessageBox(event) {
     event.target.style.height = `1px`;
     event.target.style.height = `${event.target.scrollHeight}px`;
-  }
-
-  switchConversation(event) {
-    let [data, status, xhr] = event.detail;
-    let chatContainer = this.chatContainerTarget
-    chatContainer.innerHTML = xhr.response
-    let messages = this.messagesTarget
-    messages.scrollTop = messages.scrollHeight;
-    event.target.firstElementChild.firstElementChild.classList.remove('border-red');
-  }
-
-  searchResults() {
-
   }
 }
 
