097514ba49ddc72a82f1fe0782bf5cc8c328d37e
Ife Odugbesan <ifeodugbesan@yahoo.com>
2020-06-07 21:26:07 +0100

    styling user show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,9 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    @my_posts = Post.where(user: current_user)
+    @my_tips = Tip.where(user: current_user)
+    @my_articles = Article.where(user: current_user)
     authorize @user
   end
 end
