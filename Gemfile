54dd0b9d7de7a165da6cb7c4d36ac721cdcbe879
Ife Odugbesan <ifeodugbesan@yahoo.com>
2023-01-03 01:18:51 +0000

    changed ruby version

--- Gemfile
+++ Gemfile
@@ -1,5 +1,5 @@
 source 'https://rubygems.org'
-ruby '3.1.2'
+ruby '2.7.4'
 
 gem 'bootsnap', require: false
 gem 'devise'
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
   validates :username, uniqueness: true
-  validates :email, uniqueness: true
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
