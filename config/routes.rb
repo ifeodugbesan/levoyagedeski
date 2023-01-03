96250785125eea4a3687b9ef1d46b5003a15f881
Ife Odugbesan <ifeodugbesan@yahoo.com>
2023-01-03 01:54:01 +0000

    changed ruby version to 2.7.4

--- config/routes.rb
+++ config/routes.rb
@@ -1,39 +1,20 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { omniauth_callbacks: 'callbacks' }
+  devise_for :users
+
   root to: 'pages#home'
+
   resources :posts, except: [:show] do
     resources :comments, only: [:new, :create]
     resources :likes, only: [:new, :create]
   end
+
   resources :comments, only: [:destroy]
   resources :likes, only: [:destroy]
-  resources :batches, only: [:show] do
-    resources :events, only: [:new, :create, :edit, :update]
-  end
-  resources :events, only: [:destroy]
-  resources :users, only: [:show] do
-    member do
-      patch :update_batch
-    end
-  end
-  resources :resources, except: [:show]
-  resources :articles
-  # resources :careers do
-  #   resources :project_members, only: [:new, :create]
-  # end
-  # resources :project_members, only: [:destroy] do
-  #   member do
-  #     get :accept_membership
-  #     get :reject_membership
-  #   end
-  # end
+
   resources :conversations do
     resources :messages
   end
-  resources :tips do
-    resources :upvotes, only: [:create]
-  end
-  resources :upvotes, only: [:destroy]
+
   get 'seen_pwa', to: 'pages#seen_pwa'
   get 'toggle_dark_mode', to: 'pages#toggle_dark_mode'
   get 'no_user', to: 'users#no_user'
