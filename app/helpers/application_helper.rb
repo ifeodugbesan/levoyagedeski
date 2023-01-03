f6f92a0c87d3d397f73bfd7bb498240f13df68c9
Ife Odugbesan <ifeodugbesan@yahoo.com>
2023-01-03 23:53:23 +0000

    added video capability

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def is_video?(content)
+    content.content_type.split('/')[0] == 'video'
+  end
+
+  def is_photo?(content)
+    content.content_type.split('/')[0] == 'image'
+  end
 end
