47e1b2b4d973f3c7a5e298366545bc8c8ba58230
Ife Odugbesan <ifeodugbesan@yahoo.com>
2023-01-04 12:18:35 +0000

    fixed edit profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+  validates :first_name, presence: :true
+  validates :last_name, presence: :true
   validates :username, uniqueness: true
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
